a29f1815e7eccc9219ea6a03ab99a7055333b566
LanzoInc <olanzovc@gmail.com>
2023-08-09 18:29:12 -0400

    README

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import fs from "fs";
 import path from "path";
 
 import { Command } from "commander";
+import { exec } from "child_process";
 const program = new Command();
 
 const DEFAULT_LOCAL_PATH = "C:/Users/Lanzo/Documents/Github/Mekstuff/mektemplates";
 
-async function initaction(templateName:string, targetName:string, options:{localPath: string}){
+async function initaction(templateName:string, targetName:string, options:{localPath: string, code: boolean}){
 
     const targetPath = path.join(options.localPath,templateName)
     const pathExist = fs.existsSync(targetPath)
@@ -24,10 +25,14 @@ async function initaction(templateName:string, targetName:string, options:{local
     }
     fs.cpSync(targetPath, endPath, {recursive:true});
     console.log(`[MekTemplates] cloned ${templateName}`)
+    if(options.code){
+        exec("code .", {cwd: endPath});
+    }
 }
 
 program.command("init <templateName> <targetName>")
 .option("--localPath", "local path where mektemplates files are located", DEFAULT_LOCAL_PATH)
+.option("--code", "launch in vscode ", false)
 .action(initaction)
 
 program.parse();
